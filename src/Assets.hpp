e26d9c0e762c101dfc43174f99395f6a091e7846
Michael Arcidiacono <mixarcidiacono@gmail.com>
2015-08-28 19:26:03 -0400

    having git problems, just testing

--- src/Assets.hpp
+++ src/Assets.hpp
@@ -8,7 +8,7 @@
 #define DIR_ASSETS "Assets" DIR_SEP
 #define DIR_SHADERS DIR_ASSETS "Shaders" DIR_SEP
 #define DIR_TEXTURES DIR_ASSETS "Textures" DIR_SEP
-#define DIR_SHADER_HEADER DIR_SHADERS "Header3d"
+#define DIR_SHADER_HEADER DIR_SHADERS "Header"
 #define DIR_MODELS DIR_ASSETS "Models" DIR_SEP
 
 #define DIR_VERT_EXTENSION ".vs"
